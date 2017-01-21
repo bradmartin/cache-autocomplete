2e42d08b5d2d2e581839e493486f23e42cc70edd
Brad Martin <bradwaynemartin@gmail.com>
2017-01-21 13:21:41 -0600

    init

---
+++ demo/index.js
@@ -0,0 +1,26 @@
+document.addEventListener("readystatechange", () => {
+    if (document.readyState === "complete") {
+        var autoComplete = new AutoComplete();
+        console.log(performance.now());
+        autoComplete.query('https://api.nastek.com/api/shipper/typeahead?name=al&apikey=1', true, false).then(function (resp) {
+            console.log(performance.now());
+
+            let data = JSON.parse(resp.data);
+            let optionItems = [];
+            let dataInfo;
+            for (let i = 0; i < data.length - 1; i++) {
+                optionItems[optionItems.length] = `<option value=${data[i].CST_Key}> ${data[i].CST_Name} </option>`;
+            }
+
+            let div = document.getElementById(`autoThis`);
+            let itemSelect = document.createElement("select");
+            itemSelect.style.color = "#333";
+            itemSelect.id = "myDick";
+            itemSelect.innerHTML = optionItems.join("");
+            div.appendChild(itemSelect);
+
+            console.log(performance.now());
+
+        })
+    }
+})
---
+++ src/autocomplete.ts
@@ -0,0 +1,122 @@
+let LOCAL_STORAGE: Storage;
+
+class AutoComplete {
+
+    constructor() {
+        console.log(`constructor AutoComplete`);
+    }
+
+    async query(url: string, cacheData: boolean = true, cacheBust: boolean = false): Promise<AutoCompleteHttpResponse> {
+        let result: AutoCompleteHttpResponse; /// data to return
+        let isUrlCached: boolean = false;
+
+        /// check if this url has been cached previously
+        for (let key in window.localStorage) {
+            // if (key.indexOf("autocomplete") === 1) {
+            if (key.lastIndexOf("autocomplete", 0) === 0) {
+                /// this url is cached
+                isUrlCached = true;
+            }
+        }
+
+        /// is the urlCached already and are we busting the cache
+        if (isUrlCached === true && cacheBust === true) {
+            result = await httpAsync(url);
+        } else if (isUrlCached === true && cacheBust === false) {
+            result = getCachedData(`autocomplete-${url}`);
+        } else if (isUrlCached === false) {
+            result = await httpAsync(url);
+        } else {
+            /// wtf
+            console.log(`WHAT DO WE DO?!?!?`);
+        }
+
+        if (cacheData === true && isCacheAvailable()) {
+            saveDataToCache(`autocomplete-${url}`, result);
+        }
+
+        return result;
+    }
+
+}
+
+
+/**
+ * Async XMLHttpRequest
+ */
+function httpAsync(url: string, method: string = "GET"): Promise<AutoCompleteHttpResponse> {
+    return new Promise((resolve, reject) => {
+
+        let xhr: XMLHttpRequest = new XMLHttpRequest();
+        xhr.open(method, url, true);
+
+        xhr.onload = () => {
+            let result: AutoCompleteHttpResponse = {
+                data: xhr.response,
+                status: xhr.status,
+                statusText: xhr.statusText
+            }
+            if (xhr.status >= 200 && xhr.status < 300) {
+                resolve(result);
+            } else {
+                reject(new Error(xhr.statusText));
+            }
+        }
+
+        xhr.onerror = () => {
+            reject(new Error(xhr.statusText));
+        }
+
+        xhr.send();
+    })
+}
+
+
+
+function saveDataToCache(keyName: string, data: Object): void {
+    if (keyName && data && isCacheAvailable()) {
+        try {
+            LOCAL_STORAGE.setItem(keyName, JSON.stringify(data));
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+
+function getCachedData(keyName: string) {
+    if (keyName && isCacheAvailable()) {
+        try {
+            return JSON.parse(LOCAL_STORAGE.getItem(keyName));
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+
+function isCacheAvailable(): boolean {
+    try {
+        if (window.localStorage) {
+            LOCAL_STORAGE = window.localStorage;
+            return true;
+        }
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+}
+
+
+
+interface AutoCompleteOptions {
+    // url: string;
+    prefetch: boolean;
+    cache: boolean;
+    bustCache: boolean;
+}
+
+
+interface AutoCompleteHttpResponse {
+    data: any;
+    status: number;
+    statusText: string;
+}
