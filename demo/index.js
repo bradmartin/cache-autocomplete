38fbaf288a4b2223d1f4435547c4fe69e31b761a
Brad Martin <bradwaynemartin@gmail.com>
2017-01-21 13:51:47 -0600

    Create index.js

---
+++ demo/index.js
@@ -0,0 +1,30 @@
+document.addEventListener("readystatechange", () => {
+    if (document.readyState === "complete") {
+
+        var autoComplete = new AutoComplete("autoThis");
+        console.log(performance.now()); 
+
+        autoComplete.query('https://someApi.com/api/whatever', true, false).then((resp) => {
+            console.log(performance.now());
+
+            let data = JSON.parse(resp.data);
+            let optionItems = [];
+            let dataInfo;
+            for (let i = 0; i < data.length - 1; i++) {
+                optionItems[optionItems.length] = `<option value=${data[i].CST_Key}> ${data[i].CST_Name} </option>`;
+            }
+
+            let div = document.getElementById(`autoThis`);
+            let itemSelect = document.createElement("select");
+            itemSelect.style.color = "#333";
+            itemSelect.id = "whatever";
+            itemSelect.innerHTML = optionItems.join("");
+            div.appendChild(itemSelect);
+
+            console.log(performance.now());
+
+        }, (err) => {
+            console.warn(err)
+        })
+    }
+})
